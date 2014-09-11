df5dc03bbd6ad57298f240c04605715a21186e64
jt222ii <jt222ii@student.lnu.se>
2014-09-11 10:33:07 +0200

    finished product

--- 1dv402-jt222ii-1-2-rita-med-asterisker/Program.cs
+++ 1dv402-jt222ii-1-2-rita-med-asterisker/Program.cs
@@ -10,21 +10,38 @@ namespace _1dv402_jt222ii_1_2_rita_med_asterisker
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 25; i++)
+
+            for (int rows = 0; rows < 25; rows++)
             {
-
-
-                for (int j = 0; j < 39; j++)
-                {
-                    
-
-
+                switch (rows % 3)
+                { 
+                    case 0:
+                        { 
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        }
                 }
-
-
-      
+                for (int columns = 0; columns < 39; columns++)
+                {
+                    Console.Write("* ");
+                }             
+                Console.WriteLine();
+                if (rows % 2 == 0)
+                {
+                    Console.Write(" ");
+                } 
             }
-
+           Console.ResetColor();
         }
     }
 }
